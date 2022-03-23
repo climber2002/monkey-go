02a9f69b8723f2271229ab00b0a51441d82fa0e6
Andy Wang <climber2002@gmail.com>
2022-03-23 22:28:28 +1100

    evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/climber2002/monkey-go/evaluator"
 	"github.com/climber2002/monkey-go/lexer"
 	"github.com/climber2002/monkey-go/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
