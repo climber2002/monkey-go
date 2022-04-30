5b841e3fe3625f4fec0dff00a272ba5376dd56c6
Andy Wang <climber2002@gmail.com>
2022-04-30 14:38:56 +1000

    Add STRING

--- token/token.go
+++ token/token.go
@@ -12,8 +12,9 @@ const (
 	EOF     = "EOF"
 
 	// Identitifers + literals
-	IDENT = "IDENT" // add, foobar, x, y...
-	INT   = "INT"   // 1343456
+	IDENT  = "IDENT" // add, foobar, x, y...
+	INT    = "INT"   // 1343456
+	STRING = "STRING"
 
 	// Operators
 	ASSIGN   = "="
